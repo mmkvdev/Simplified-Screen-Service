19ea083000cf01263bdab971783ba1f3e178f2ae
mmkvdev <madhuv299@gmail.com>
2020-07-04 23:43:01 +0530

    Update: App.js Update

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 import SimplifiedDrawerService from './components/SimplifiedDrawerService';
+import CallMeScreen from './components/CallMeScreen';
 
 export default function App() {
   return (
     <View>
-      <SimplifiedDrawerService />
+      <CallMeScreen />
     </View>
   );
 }

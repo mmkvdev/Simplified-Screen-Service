c65cf882598800abdc81f10585ff7d5ff914a1c2
mmkvdev <madhuv299@gmail.com>
2020-07-05 22:09:45 +0530

    Update: chores 🥴

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { View } from 'react-native';
 import SimplifiedDrawerService from './components/SimplifiedDrawerService';
 import CallMeScreen from './components/CallMeScreen';
+import CallMeScreenStackDrawer from './stackdrawers/CallMeScreenStackDrawer';
 
 export default function App() {
   return (
     <View>
-      <CallMeScreen />
+      <SimplifiedDrawerService />
     </View>
   );
 }
